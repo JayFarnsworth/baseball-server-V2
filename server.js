7f75106001b283383ac7ec8544bba3b28f6f4900
Jay "Danger" Farnsworth <DrDanger@Jays-MacBook-Pro.local>
2018-04-02 14:32:55 -0600

    m

--- server.js
+++ server.js
@@ -228,7 +228,7 @@ app.get('/games', function (req, res) {
   } else {
     var date = getDate();
   }
-  let url = 'https://api.mysportsfeeds.com/v1.2/pull/mlb/2018-regular/daily_game_schedule.json?fordate=20170920';
+  let url = 'https://api.mysportsfeeds.com/v1.2/pull/mlb/2018-regular/daily_game_schedule.json?fordate=' + date;
   fetch(url, {
     credentials: 'same-origin',
     headers: MSFHeaders,
